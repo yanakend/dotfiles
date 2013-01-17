1f2abfcce7037b1bd2a8c682fea017ff36e74061
yanagi-mba <yanagikenji@yanagi-mba.local>
2013-01-18 01:30:53 +0900

    一旦整理

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+install_path=$HOME
+installer_path=`pwd`
+cd ${installer_path}
+git submodule init
+git submodule update
+
+install_file()
+{
+	local _target=$1
+	local _linkname=$2
+	if [ ! -e "${_linkname}" ]; then
+		ln -s "${_target}" "${_linkname}"
+		echo "linking ${_linkname}"
+	else
+		echo "skipping ${_linkname}"
+	fi
+}
+
+dotfiles=( .bashrc .zshrc .vim .vimrc .gvimrc )
+
+for file in ${dotfiles[@]}
+do
+	install_file "${installer_path}/${file}" "${install_path}/${file}"
+done
