1e50dfbb79b00355e1b69bd1c47effdfdda47181
unknown <X00002@CAX-0150.cyberagent.co.jp>
2013-01-18 05:03:50 +0900

    win support

---
+++ install.bat
@@ -0,0 +1,12 @@
+git clone git://github.com/Shougo/neobundle.vim.git %CD%\.vim\bundle\neobundle.vim
+
+
+mklink %USERPROFILE%\.bashrc %CD%\.bashrc
+mklink %USERPROFILE%\.bashrc_profile %CD%\.bashrc_profile
+mklink %USERPROFILE%\.zshrc %CD%\.zshrc
+mklink /d %USERPROFILE%\.vim %CD%\.vim
+mklink %USERPROFILE%\.vimrc %CD%\.vimrc
+mklink %USERPROFILE%\.gvimrc %CD%\.gvimrc
+
+
+copy %CD%\.vim\bundle\vimdoc-ja\doc\ %USERPROFILE%\.vim\doc
