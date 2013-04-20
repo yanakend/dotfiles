66d9e5ca7e5d9a5cf67bac685d387b13db4aa9f6
yanaken <yanaken@www37011u.sakura.ne.jp>
2013-04-20 17:59:21 +0900

    install.sh編集

--- install.sh
+++ install.sh
@@ -6,27 +6,27 @@ cd ${installer_path}
 #git submodule init
 #git submodule update
 
-git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
-git clone git://github.com/vim-jp/vimdoc-ja.git ~/.vim/bundle/vimdoc-ja
-
-
 install_file()
 {
 	local _target=$1
 	local _linkname=$2
-	if [ ! -e "${_linkname}" ]; then
-		ln -s "${_target}" "${_linkname}"
-		echo "linking ${_linkname}"
-	else
-		echo "skipping ${_linkname}"
+	if [ -e "${_linkname}" ]; then
+		mv -f "${_linkname}" "${_linkname}.org"
 	fi
+	ln -s "${_target}" "${_linkname}"
+	echo "linking ${_linkname}"
 }
 
-dotfiles=( .bashrc_profile .bashrc .zshrc .vim .vimrc .gvimrc )
+dotfiles=( .bash_profile .bashrc .zshrc .vim .vimrc .gvimrc )
 
 for file in ${dotfiles[@]}
 do
 	install_file "${installer_path}/${file}" "${install_path}/${file}"
 done
 
+git clone git://github.com/Shougo/neobundle.vim.git ${installer_path}/.vim/bundle/neobundle.vim
+git clone git://github.com/vim-jp/vimdoc-ja.git ${installer_path}/.vim/bundle/vimdoc-ja
+
 cp -r ~/.vim/bundle/vimdoc-ja/doc/* ~/.vim/doc
+
+vim -N -u NONE -i NONE -V1 -e -s --cmd "source ${installer_path}/.vimrc" --cmd NeoBundleInstall! --cmd qall!
